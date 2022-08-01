758c142e7492edd20d19e9facf26fc467722ffba
Ghaarp <xdmail@bk.ru>
2022-08-01 20:04:33 +0300

    changes for heroku deploy

--- src/db.js
+++ src/db.js
@@ -1,8 +1,16 @@
 const { Sequelize } = require("sequelize");
 const env = process.env;
 
-module.exports = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASSWORD, {
+/*module.exports = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASSWORD, {
   dialect: "postgres",
   host: env.DB_HOST,
   port: env.DB_PORT,
+});*/
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
